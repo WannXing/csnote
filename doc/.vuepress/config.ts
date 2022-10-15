d37dea3d36efa1f5204fd0b0df2c750bb33c61b3
Wannxing <wanxing0122@163.com>
2022-10-15 22:13:47 +0800

    update

--- doc/.vuepress/config.ts
+++ doc/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { hopeTheme } from "vuepress-theme-hope";
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { searchConsolePlugin } from 'vuepress-plugin-china-search-console'
 import theme from "./theme";
 
@@ -12,6 +13,28 @@ export default defineUserConfig({
   base: "/csnote/",
   theme,
   plugins: [
+    docsearchPlugin({
+      apiKey: '<API_KEY>',
+      indexName: '<INDEX_NAME>',
+      locales: {
+        '/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: 'Search Documentation',
+            },
+          },
+        },
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+            },
+          },
+        },
+      },
+    }),
     searchConsolePlugin({
       // options ...
       baiduId :"https://hm.baidu.com/hm.js?b6d599c072da173be238f2bfe83a025b",
