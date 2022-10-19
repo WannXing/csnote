937f2a80b8eb0cc6a49febe0b30a419afbc8a5bb
WannXing <106435147+WannXing@users.noreply.github.com>
2022-10-19 13:40:17 +0000

    Deploying to gh-pages from @ WannXing/csnote@0abe9f4969263d5889a13b81c1057db9b694392d 🚀

---
+++ assets/index.html.84aa34b2.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper.21dcd24c.js";import{o,c as n,d as e}from"./app.594f4702.js";const r={},s=e("p",null,[e("em",null,[e("strong",null,"Wish you a better day.")])],-1),_=[s];function c(l,a){return o(),n("div",null,_)}var m=t(r,[["render",c],["__file","index.html.vue"]]);export{m as default};
